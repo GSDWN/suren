5e00bfb4d5011f22fa6f9e01b474f8e2edf81fed
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-12-29 20:09:39 +0100

    change main config for make app runnable

--- src/main/java/com/artronics/gsdwn/suren/SurenApplication.java
+++ src/main/java/com/artronics/gsdwn/suren/SurenApplication.java
@@ -1,12 +1,21 @@
 package com.artronics.gsdwn.suren;
 
+import com.artronics.gsdwn.suren.persistence.PersistenceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-public class SurenApplication {
+@Import(PersistenceConfig.class)
+@ComponentScan(basePackages = {
+        "com.artronics.gsdwn.suren.persistence",
+})
+public class SurenApplication
+{
 
-	public static void main(String[] args) {
-		SpringApplication.run(SurenApplication.class, args);
-	}
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SurenApplication.class, args);
+    }
 }
